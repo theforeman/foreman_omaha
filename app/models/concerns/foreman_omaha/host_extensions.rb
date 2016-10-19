67e10238319749fafee6c516eb732116589e8437
Timo Goebel <timo.goebel@dm.de>
2016-10-19 17:20:32 +0200

    delete omaha reports when deleting a host

--- app/models/concerns/foreman_omaha/host_extensions.rb
+++ app/models/concerns/foreman_omaha/host_extensions.rb
@@ -3,7 +3,7 @@ module ForemanOmaha
     extend ActiveSupport::Concern
     included do
       has_many :omaha_reports, :class_name => '::ForemanOmaha::OmahaReport',
-                               :foreign_key => :host_id
+                               :foreign_key => :host_id, :dependent => :destroy
       has_one :last_omaha_report_object, -> { order("#{Report.table_name}.id DESC") },
               :foreign_key => :host_id, :class_name => '::ForemanOmaha::OmahaReport'
     end
