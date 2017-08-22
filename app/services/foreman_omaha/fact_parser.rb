a1b4e8ad7b9ed5bb15f7900b7b616bcf9050969e
Timo Goebel <mail@timogoebel.name>
2017-08-22 07:54:33 +0200

    copy previous os attributes

--- app/services/foreman_omaha/fact_parser.rb
+++ app/services/foreman_omaha/fact_parser.rb
@@ -3,9 +3,9 @@ module ForemanOmaha
     def operatingsystem
       args = { :name => facts['platform'], :major => facts['osmajor'], :minor => facts['osminor'] }
       description = "#{facts['platform']} #{facts['version']}"
-      Operatingsystem.where(args).first ||
-        Operatingsystem.create!(args.merge(:description => description,
-                                           :release_name => facts['track']))
+      Coreos.where(args).first ||
+        create_coreos_version(args.merge(:description => description,
+                                         :release_name => facts['track']))
     end
 
     def architecture
@@ -27,6 +27,22 @@ module ForemanOmaha
 
     def support_interfaces_parsing?
       false
+    end
+
+    private
+
+    def create_coreos_version(attrs)
+      previous_version = previous_coreos_version
+      return Coreos.create!(attrs) unless previous_coreos_version
+      os = previous_version.deep_clone(
+        :include => [:ptables, :media, :os_default_templates, :architectures]
+      )
+      os.update_attributes(attrs)
+      os
+    end
+
+    def previous_coreos_version
+      Coreos.all.sort_by { |os| Gem::Version.new(os.release) }.last
     end
   end
 end
---
+++ test/unit/omaha_fact_parser_test.rb
@@ -0,0 +1,77 @@
+require 'test_helper'
+
+class OmahaFactsParserTest < ActiveSupport::TestCase
+  setup do
+    User.current = users(:admin)
+  end
+
+  let(:facts) do
+    {
+      'appid' => 'e96281a6-d1af-4bde-9a0a-97b76e56dc57',
+      'version' => '1068.9.0',
+      'track' => 'stable',
+      'board' => 'amd64-usr',
+      'alephversion' => '1010.5.0',
+      'oemversion' => '',
+      'oem' => '',
+      'platform' => 'CoreOS',
+      'osmajor' => '1068',
+      'osminor' => '9.0',
+      'ipaddress' => '127.0.0.1',
+      'ipaddress6' => '',
+      'hostname' => 'client.example.com'
+    }
+  end
+  let(:importer) { ::ForemanOmaha::FactParser.new(facts) }
+
+  context '#architecture' do
+    let(:architecture) { importer.architecture }
+
+    test 'should return an Architecture' do
+      assert_kind_of Architecture, architecture
+      assert_equal Architecture.find_by(name: 'x86_64'), architecture
+    end
+  end
+
+  context '#operatingsystem' do
+    let(:os) { importer.operatingsystem }
+
+    test 'should return an OS with correct params' do
+      assert_kind_of Operatingsystem, os
+      assert_equal '1068', os.major
+      assert_equal '9.0', os.minor
+      assert_equal 'CoreOS', os.name
+      assert_equal 'CoreOS 1068.9.0', os.description
+      assert_equal 'stable', os.release_name
+      assert_empty os.ptables
+      assert_empty os.media
+      assert_empty os.os_default_templates
+    end
+
+    context 'with old versions' do
+      setup do
+        FactoryBot.create(:coreos,
+                          :with_associations,
+                          :with_os_defaults,
+                          :major => '899',
+                          :minor => '17.0',
+                          :title => 'CoreOS 899.17.0')
+        @previous = FactoryBot.create(:coreos,
+                                      :with_associations,
+                                      :with_os_defaults,
+                                      :major => '1010',
+                                      :minor => '5.0',
+                                      :title => 'CoreOS 1010.5.0')
+      end
+
+      test 'should copy attributes from previous os version' do
+        assert_equal '1068', os.major
+        assert_equal '9.0', os.minor
+        assert_equal @previous.ptables, os.ptables
+        assert_equal @previous.architectures, os.architectures
+        assert_equal @previous.media, os.media
+        assert_equal @previous.os_default_templates, os.os_default_templates
+      end
+    end
+  end
+end
