1e1878d511e1bd91becd67ab16353b500bf2ccdb
Timo Goebel <mail@timogoebel.name>
2017-11-06 13:06:01 +0100

    replace FactoryGirl with FactoryBot

--- test/functional/api/v2/omaha_reports_controller_test.rb
+++ test/functional/api/v2/omaha_reports_controller_test.rb
@@ -12,7 +12,7 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
   end
 
   test 'should get index' do
-    FactoryGirl.create(:omaha_report)
+    FactoryBot.create(:omaha_report)
     get :index, {}
     assert_response :success
     assert_not_nil assigns(:omaha_reports)
@@ -21,7 +21,7 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
   end
 
   test 'should show individual record' do
-    report = FactoryGirl.create(:omaha_report)
+    report = FactoryBot.create(:omaha_report)
     get :show, :id => report.to_param
     assert_response :success
     show_response = ActiveSupport::JSON.decode(@response.body)
@@ -29,7 +29,7 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
   end
 
   test 'should destroy report' do
-    report = FactoryGirl.create(:omaha_report)
+    report = FactoryBot.create(:omaha_report)
     assert_difference('ForemanOmaha::OmahaReport.count', -1) do
       delete :destroy, :id => report.to_param
     end
@@ -38,7 +38,7 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
   end
 
   test 'should get reports for given host only' do
-    report = FactoryGirl.create(:omaha_report)
+    report = FactoryBot.create(:omaha_report)
     get :index, :host_id => report.host.to_param
     assert_response :success
     assert_not_nil assigns(:omaha_reports)
@@ -48,7 +48,7 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
   end
 
   test 'should return empty result for host with no reports' do
-    host = FactoryGirl.create(:host)
+    host = FactoryBot.create(:host)
     get :index, :host_id => host.to_param
     assert_response :success
     assert_not_nil assigns(:omaha_reports)
@@ -58,7 +58,7 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
   end
 
   test 'should get last report' do
-    reports = FactoryGirl.create_list(:omaha_report, 5)
+    reports = FactoryBot.create_list(:omaha_report, 5)
     get :last, set_session_user
     assert_response :success
     assert_not_nil assigns(:omaha_report)
@@ -68,8 +68,8 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
   end
 
   test 'should get last report for given host only' do
-    main_report = FactoryGirl.create(:omaha_report)
-    FactoryGirl.create_list(:omaha_report, 5)
+    main_report = FactoryBot.create(:omaha_report)
+    FactoryBot.create_list(:omaha_report, 5)
     get :last, { :host_id => main_report.host.to_param }, set_session_user
     assert_response :success
     assert_not_nil assigns(:omaha_report)
@@ -79,13 +79,13 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
   end
 
   test 'should give error if no last report for given host' do
-    host = FactoryGirl.create(:host)
+    host = FactoryBot.create(:host)
     get :last, :host_id => host.to_param
     assert_response :not_found
   end
 
   test 'cannot view the last report without hosts view permission' do
-    report = FactoryGirl.create(:report)
+    report = FactoryBot.create(:report)
     setup_user('view', 'omaha_reports')
     get :last, { :host_id => report.host.id }, set_session_user.merge(:user => User.current.id)
     assert_response :not_found
@@ -93,14 +93,14 @@ class Api::V2::OmahaReportsControllerTest < ActionController::TestCase
 
   describe 'unpriveliged user' do
     def setup
-      User.current = FactoryGirl.create(:user, :admin)
+      User.current = FactoryBot.create(:user, :admin)
     end
 
     test 'hosts with a registered smart proxy on should create a report successfully' do
       Setting[:restrict_registered_smart_proxies] = true
       Setting[:require_ssl_smart_proxies] = false
 
-      proxy = FactoryGirl.create(:smart_proxy, :omaha)
+      proxy = FactoryBot.create(:smart_proxy, :omaha)
       host = URI.parse(proxy.url).host
       Resolv.any_instance.stubs(:getnames).returns([host])
       as_user :one do
