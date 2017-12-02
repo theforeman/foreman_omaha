588d5f1595f24925cf0d7de857520f48635f3ed9
Timo Goebel <mail@timogoebel.name>
2017-12-02 20:52:10 +0100

    add status description text

--- app/services/foreman_omaha/status_mapper.rb
+++ app/services/foreman_omaha/status_mapper.rb
@@ -24,5 +24,30 @@ module ForemanOmaha
         _('unknown')
       end
     end
+
+    # rubocop:disable Metrics/CyclomaticComplexity
+    def to_description(version = nil)
+      case status.to_sym
+      when :complete
+        if version.present?
+          _('The host has been updated successfully and is now running version %s.') % version
+        else
+          _('The host has been updated successfully.')
+        end
+      when :downloading
+        _('The host has just started downloading the update package.')
+      when :downloaded
+        _('The host has downloaded the update package and will install it now.')
+      when :installed
+        _('The host has installed the update package but is not using it yet.')
+      when :instance_hold
+        _('An update for this host is pending but it was put on hold because of the rollout policy.')
+      when :error
+        _('The host reported an error while updating.')
+      else
+        _('The status of this host is unknown.')
+      end
+    end
+    # rubocop:enable Metrics/CyclomaticComplexity
   end
 end
