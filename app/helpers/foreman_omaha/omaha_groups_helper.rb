513a3f576a508c9622e06fd1265bdca6e63a479f
Timo Goebel <mail@timogoebel.name>
2017-12-02 22:49:03 +0100

    add version breakdown bar

---
+++ app/helpers/foreman_omaha/omaha_groups_helper.rb
@@ -0,0 +1,52 @@
+module ForemanOmaha
+  module OmahaGroupsHelper
+    def omaha_version_breakdown_bar(omaha_group)
+      stylesheet 'foreman_omaha/version_breakdown'
+      colors = {
+        :success => ['#3f9c35', '#6ec664', '#2d7623'],
+        :warning => ['#ec7a08', '#f7bd7f', '#f39d3c'],
+        :danger => ['#cc0000', '#a30000', '#8b0000', '#470000']
+      }
+      versions = ForemanOmaha::GroupVersionBreakdown.new(:omaha_group => omaha_group).version_breakdown
+      version_list = omaha_group.operatingsystems.pluck(:major, :minor).map { |v| Gem::Version.new(v.join('.')) }.sort.reverse
+      current = version_list.max
+      content_tag :div, :class => 'progress version-breakdown' do
+        safe_join(versions.map do |version|
+          semver = Gem::Version.new(version[:version])
+          position = version_list.index(semver)
+          css = {
+            :width => "#{version[:percentage]}%"
+          }
+          label = if semver > current
+                    :info
+                  elsif semver == current
+                    :success
+                  elsif position == 1
+                    :warning
+                  else
+                    :danger
+                  end
+          if colors.key?(label)
+            color = colors[label].first
+            colors[label].rotate!
+            css[:'background-color'] = color
+          end
+          content_tag :div,
+                      :class => "progress-bar progress-bar-#{label}",
+                      :role => 'progressbar',
+                      :title => version[:version],
+                      :style => css_style(css) do
+            link_to(
+              version[:version],
+              hosts_path(:search => "omaha_group = #{omaha_group} and omaha_version = #{version[:version]}")
+            )
+          end
+        end)
+      end
+    end
+
+    def css_style(hash)
+      hash.map { |k, v| "#{k}: #{v};" }.join(' ')
+    end
+  end
+end
---
+++ test/unit/group_version_breakdown_test.rb
@@ -0,0 +1,65 @@
+require 'test_plugin_helper'
+
+class CurrentVersionBreakdownTest < ActiveSupport::TestCase
+  setup do
+    User.current = FactoryBot.create(:user, :admin)
+  end
+
+  let(:stable_group) { FactoryBot.create(:omaha_group, :uuid => 'stable', :name => 'Stable') }
+  let(:beta_group) { FactoryBot.create(:omaha_group, :uuid => 'beta', :name => 'Beta') }
+  let(:group_version_breakdown) { ForemanOmaha::GroupVersionBreakdown.new(:omaha_group => stable_group) }
+  let(:stable_releases) { ['1520.6.0', '1520.5.0', '1465.8.0'] }
+  let(:beta_coreos) { create_coreos_os('1576.3.0', 'beta') }
+
+  context 'with hosts' do
+    setup do
+      oslist = stable_releases.map do |release|
+        create_coreos_os(release)
+      end
+      FactoryBot.create_list(
+        :host, 2, :managed, :with_omaha_facet,
+        :omaha_version => oslist.first.release,
+        :omaha_group => stable_group,
+        :operatingsystem => oslist.first
+      )
+      FactoryBot.create(
+        :host, :managed, :with_omaha_facet,
+        :omaha_version => oslist.last.release,
+        :omaha_group => stable_group,
+        :operatingsystem => oslist.last
+      )
+      FactoryBot.create(
+        :host, :managed, :with_omaha_facet,
+        :omaha_version => beta_coreos.release,
+        :omaha_group => beta_group,
+        :operatingsystem => beta_coreos
+      )
+    end
+
+    test 'counts host in stable group' do
+      assert_equal 3, group_version_breakdown.facets.count
+    end
+
+    test 'calculates a version breakdown' do
+      expected = [
+        { :version => '1520.6.0', :count => 2, :percentage => 66.67 },
+        { :version => '1465.8.0', :count => 1, :percentage => 33.33 }
+      ]
+      assert_equal expected, group_version_breakdown.version_breakdown
+    end
+  end
+
+  private
+
+  def create_coreos_os(version, channel = 'stable')
+    semver = Gem::Version.new(version)
+    FactoryBot.create(
+      :coreos,
+      :with_associations,
+      :major => semver.segments.first,
+      :minor => semver.segments.last(2).join('.'),
+      :release_name => channel,
+      :title => "CoreOS #{version}"
+    )
+  end
+end
