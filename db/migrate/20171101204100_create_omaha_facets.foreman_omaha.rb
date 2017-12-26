f67d18cda319281402538e0b6f9e34a193a3166f
Timo Goebel <mail@timogoebel.name>
2017-12-26 20:05:11 +0100

    set DB migration compatibility to 4.2

--- db/migrate/20171101204100_create_omaha_facets.foreman_omaha.rb
+++ db/migrate/20171101204100_create_omaha_facets.foreman_omaha.rb
@@ -1,4 +1,4 @@
-class CreateOmahaFacets < ActiveRecord::Migration
+class CreateOmahaFacets < ActiveRecord::Migration[4.2]
   def change
     create_table :omaha_groups do |t|
       t.string :name, limit: 255, null: false, index: true
