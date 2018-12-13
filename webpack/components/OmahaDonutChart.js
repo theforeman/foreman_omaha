3504f2d1430b7adfe8ee6f4b276e9210c73b05be
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2018-12-13 09:40:39 +0100

    Change eslint config

--- webpack/components/OmahaDonutChart.js
+++ webpack/components/OmahaDonutChart.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import DonutChart from 'foremanReact/components/common/charts/DonutChart';
 
-const OmahaDonutChart = ({
-  data
-}) => {
-  const { columns, search } = data;
-  return <DonutChart data={columns} config='small' onclick={(e) => {
-    if(search && search[e.id]) {
-      window.location.href = search[e.id]
-    }
-  }} />
-}
+const OmahaDonutChart = ({ data: { columns, search } }) => (
+  <DonutChart
+    data={columns}
+    config="small"
+    onclick={e => {
+      if (search && search[e.id]) {
+        window.location.href = search[e.id];
+      }
+    }}
+  />
+);
+
+OmahaDonutChart.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default OmahaDonutChart;
